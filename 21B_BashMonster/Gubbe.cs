d05966d265c8da0e1311b4bf0a301dba67912390
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-09-01 10:04:47 +0200

    Initial commit

---
+++ 21B_BashMonster/Gubbe.cs
@@ -0,0 +1,7 @@
+﻿namespace BashMonster;
+
+public class Gubbe
+{
+  public float hp = 100;
+  public float mass = 80;
+}
---
+++ 21B_BashMonster/Program.cs
@@ -0,0 +1,12 @@
+﻿using BashMonster;
+
+Gubbe martin = new();
+Gubbe micke = new();
+
+martin.hp = 100;
+micke.hp = 60;
+
+Console.ReadLine();
+
+//Metoder
+//Konstruktorer
